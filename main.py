e8bcf35e3c391d29a0fe40e8b2209843ecf390e0
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2020-12-14 21:51:14 +0100

    Add constructors

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 class Product:
-    pass
+    def __init__(self, name, product_category, unit_price):
+        self.name = name
+        self.product_category = product_category
+        self.unit_price = unit_price
+
 
 class Order:
-    pass
+    def __init__(self, client_firstname, client_lastname, products = None):
+        self.client_firstname = client_firstname
+        self.client_lastname = client_lastname
+        self.total_price = 0
+
+        if products == None:
+            products = []
+        self.products = products
+
+        for product in products:
+            self.total_price += product.unit_price
+
 
 class Apple:
-    pass
+    def __init__(self, species_name, size, price):
+        self.species_name = species_name
+        self.size = size
+        self.price = price
+
 
 class Potato:
-    pass
-
-orders = [Order(), Order(), Order(), Order(), Order()]
-
-products = {
-    "komputer": Product(),
-    "monitor": Product()
-}
+    def __init__(self, species_name, size, price):
+        self.species_name = species_name
+        self.size = size
+        self.price = price
 
 
 if __name__ == '__main__':
